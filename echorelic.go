8ae287c21d4d03ca882dd3daa9a2bcdf15ec3736
Jessie Barnett <jessie.lynn.barnett@gmail.com>
2017-08-11 16:21:34 -0400

    GoDoc Comments

--- echorelic.go
+++ echorelic.go
@@ -5,11 +5,13 @@ import (
 	"github.com/newrelic/go-agent"
 )
 
+// EchoRelic contains the connection to New Relic
 type EchoRelic struct {
 	app  newrelic.Application
 	name string
 }
 
+// Init is used to create a newrelic.Application
 func (r *EchoRelic) Init(appName string, licenseKey string) (newrelic.Application, error) {
 	config := newrelic.NewConfig(appName, licenseKey)
 	app, err := newrelic.NewApplication(config)
@@ -18,6 +20,7 @@ func (r *EchoRelic) Init(appName string, licenseKey string) (newrelic.Applicatio
 	return app, err
 }
 
+// EchoRelicMiddleware starts a newrelic.Transactions and adds it to the request's echo.Context
 func (r *EchoRelic) EchoRelicMiddleware() echo.MiddlewareFunc {
 	return func(h echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
