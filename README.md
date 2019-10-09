1bcd3c8c5bdfb245c72e6739f458038d743fed65
Jessie Barnett <jessie.lynn.barnett@gmail.com>
2019-10-09 00:05:39 -0400

    Update middleware style (#8)
    * Update middleware style
    
    * Fix travis config
    
    * Test for config failure
    
    * Test middleware

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Echo middleware for [New Relic](https://newrelic.com/)
 
 [![GoDoc](https://godoc.org/github.com/jessie-codes/echo-relic?status.svg)](https://godoc.org/github.com/jessie-codes/echo-relic)
 [![Build Status](https://travis-ci.org/jessie-codes/echo-relic.svg?branch=master)](https://travis-ci.org/jessie-codes/echo-relic)
+[![Coverage Status](https://coveralls.io/repos/github/jessie-codes/echo-relic/badge.svg?branch=master)](https://coveralls.io/github/jessie-codes/echo-relic?branch=master)
 
 ## install
 
@@ -10,9 +11,7 @@ Echo middleware for [New Relic](https://newrelic.com/)
 
 ## Usage
 
-Echo Relic starts a new transaction for each request and binds the transaction
-to the request context. See [go-agent](https://github.com/newrelic/go-agent)'s
-documentation for how to use the transaction interface.
+Echo Relic starts a new transaction for each request, binds the transaction to the request context, and end the transaction after handling has been completed. It uses the following convention for naming transaction: `<Method> <Path>`. It automatically adds attributes for `RealIP`, `IsTLS`, `IsWebSocket`, and `Query`. See [go-agent](https://github.com/newrelic/go-agent)'s documentation for how to use the transaction interface.
 
 ```golang
 
@@ -26,9 +25,8 @@ import (
 
 func main() {
 	e := echo.New()
-	config := newrelic.NewConfig("__APP_NAME__", "__NEW_RELIC_LICENSE_KEY__")
-	app, _ := newrelic.NewApplication(config)
-	e.Use(echorelic.Middleware(app))
+	relic = echorelic.New("__APP_NAME__", "__NEW_RELIC_LICENSE_KEY__")
+	e.Use(relic.Transaction)
 
 	e.GET("/", func(c echo.Context) error {
 		txn := c.Get("newRelicTransaction")
--- echorelic_test.go
+++ echorelic_test.go
@@ -1,40 +1,58 @@
 package echorelic
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/labstack/echo"
-	"github.com/newrelic/go-agent"
+	newrelic "github.com/newrelic/go-agent"
 	"github.com/stretchr/testify/suite"
 )
 
 type TestSuite struct {
 	suite.Suite
-	app newrelic.Application
-	e   *echo.Echo
+	echoRelic *EchoRelic
+	e         *echo.Echo
 }
 
 func (suite *TestSuite) SetupTest() {
-	config := newrelic.NewConfig("test", "1234567890123456789012345678901234567890")
-	app, _ := newrelic.NewApplication(config)
-	suite.app = app
-	suite.e = echo.New()
-}
-
-func (suite *TestSuite) TestMiddleware() {
-	m := Middleware(suite.app)
-	suite.NotEmpty(m)
+	echoRelic, err := New("test", "1234567890123456789012345678901234567890")
+	if err != nil {
+		suite.Fail("Failed to create new EchoRelic")
+	}
+	suite.echoRelic = echoRelic
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	suite.e = e
 }
 
 func (suite *TestSuite) TestUseMiddleware() {
 	var t newrelic.Transaction
-	suite.e.Use(Middleware(suite.app))
+	suite.e.Use(suite.echoRelic.Transaction)
 	req := httptest.NewRequest(echo.GET, "/", nil)
 	res := httptest.NewRecorder()
 	c := suite.e.NewContext(req, res)
 	txn := c.Get("newRelicTransaction")
 	suite.IsType(t, txn)
+}
+
+func (suite *TestSuite) TestBadConfig() {
+	_, err := New("test", "1234567890")
+	suite.Error(err, "Should error when a key is passed with an invalid length")
+}
+
+func (suite *TestSuite) TestShouldReturnNil() {
+	handlerFunc := suite.echoRelic.Transaction(func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	req := httptest.NewRequest(echo.GET, "/", nil)
+	res := httptest.NewRecorder()
+	c := suite.e.NewContext(req, res)
+	err := handlerFunc(c)
+	suite.NoError(err)
 }
 
 func TestMethodSuite(t *testing.T) {
